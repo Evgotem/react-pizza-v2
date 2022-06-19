875e5ddd285e92f8e8bfca3184c9d1e0c733af29
Evgeny Temryazansky <evgenytemr@gmail.com>
2022-06-19 23:45:23 +0400

    урок 4, рендер пицц, выбор категорий и типа теста

---
+++ src/components/Categories.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Categories() {
+  const [activeCategory, setActiveCategory] = React.useState(0);
+
+  const handleCategoryClick = (index) => {
+    setActiveCategory(index);
+    console.log(index);
+  };
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((category, index) => (
+          <li 
+            key={category} 
+            onClick={() => handleCategoryClick(index)}
+            className={index === activeCategory ? 'active': ''}
+          >
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
